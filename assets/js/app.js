3767a3802f1843ca843daeb01af4417abbac7808
Sebastian Rojo <seba.rojo@duocuc.cl>
2026-09-18 20:52:09 -0300

    feat: construir estructura semántica de inicio y nosotros

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,7 @@
 const botonBienvenida = document.querySelector("#boton-bienvenida");
 
-botonBienvenida.addEventListener("click", () => {
-  alert("Proyecto semestral: Conecta Cultura paso a paso.");
-});
+if (botonBienvenida) {
+  botonBienvenida.addEventListener("click", () => {
+    alert("Conecta Cultura se construye paso a paso.");
+  });
+}
